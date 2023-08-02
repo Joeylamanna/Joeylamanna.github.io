e2fbd3a0f8d7fe442ba892576adfc22becbf28d2
Joeylamanna <josephlamanna03@gmail.com>
2023-08-02 15:56:02 -0400

    updated home page component with single line typing so it looks like a person is not typing multiple lines at once. Also updated about page with animations to look nicer on loading the page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 const About = () => {
+    
+        const [letterClass, setLetterClass] = useState('text-animate')
+      
+
+useEffect(() => {
+        
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+        
+        return () => {
+                    clearTimeout(timeoutId)
+                }
+    }, [])
+
     return (
         <div className='container about-page'>
             <div className='text-zone'> 
-                <h1>About me</h1>
+                <h1>
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={['A', 'b', 'o','u','t',' ','m','e']}
+                        idx = {15}
+                    />
+                    
+                    
+                </h1>
                 <p> I am a Computer Science student in my junior year
                     at Villanova University with an interest
                     in coding, cybersecurity, web development,
