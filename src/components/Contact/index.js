72b421aa803829e05db91f787b9886008eb56cbf
unknown <jlamanna@villanova.edu>
2023-08-04 10:53:49 -0400

    fixed loader on contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,13 @@
 import './index.scss'
+import Loader from 'react-loaders'
 const Contact = () => {
     return (
-        
+        <> 
         <div class="contact">
         <h1>This is a contact page</h1>
         </div>
+        <Loader type= "pacman"/>
+        </>
     )
 }
 
